4500d95f681a2b881fb21164201d94e0324ea072
ppmaungdev <ppmaungdev@gmail.com>
2023-02-03 15:23:25 +0630

    init porj and add interfaces

---
+++ type.ts
@@ -0,0 +1,39 @@
+export interface Pokemon {
+    id: {}
+    number: String
+    name: String
+    weight: PokemonDimension
+    height: PokemonDimension
+    classification: string
+    types: string[]
+    resistant: string[]
+    attacks: PokemonAttack
+    weaknesses: string[]
+    fleeRate: number
+    maxCP: number
+    evolutions: Pokemon[] 
+    evolutionRequirements: PokemonEvolutionRequirement
+    maxHP: number
+    image: string
+}
+
+export interface PokemonDimension {
+    minimum: String,
+    maximum: String
+}
+
+export interface PokemonAttack {
+    fast: Attack[]
+    special: Attack[]
+}
+
+export interface Attack {
+    name: string
+    type: string
+    damage: number
+}
+
+export interface PokemonEvolutionRequirement {
+    amount: number
+    name: string
+}
